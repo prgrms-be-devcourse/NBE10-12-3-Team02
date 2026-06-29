4ab31cedc5cb4fa93fdba3a693eaee78e8b1875c
bangyewon <90364660+bangyewon@users.noreply.github.com>
2026-06-29 17:20:43 +0900

    Feat: ErrorCode 공용 구조 추가 (#37)
    * Feat: 에러코드 구조 추가 #36
    
    * Refactor: status 관련 코드 제거 #36
    
    * Refactor: errorCode 중복 제거 #36

---
+++ back/src/main/java/com/back/global/exception/ErrorCode.java
@@ -0,0 +1,53 @@
+package com.back.global.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+    // Auth
+    AUTH_ID_NOT_FOUND("401-1", "존재하지 않는 아이디입니다."),
+    AUTH_PASSWORD_MISMATCH("401-2", "비밀번호가 일치하지 않습니다."),
+    AUTH_INVALID_BEARER_HEADER("401-3", "Authorization 헤더가 Bearer 형식이 아닙니다."),
+    AUTH_INVALID_CREDENTIALS("401-4", "인증 정보가 유효하지 않습니다."),
+    AUTH_INVALID_REFRESH_TOKEN("401-5", "유효하지 않은 리프레시 토큰입니다."),
+    AUTH_REFRESH_TOKEN_NOT_FOUND_IN_REDIS("401-6", "Redis에 리프레시 토큰이 존재하지 않습니다."),
+    AUTH_REFRESH_TOKEN_MISMATCH("401-7", "리프레시 토큰이 일치하지 않습니다."),
+    AUTH_LOGIN_REQUIRED("401-8", "로그인 후 이용해주세요."),
+    AUTH_FORBIDDEN("403-1", "권한이 없습니다."),
+
+    // User
+    USER_NOT_FOUND("404-1", "회원이 존재하지 않습니다."),
+    USER_ID_ALREADY_EXISTS("409-1", "이미 사용 중인 아이디입니다."),
+    USER_EMAIL_ALREADY_EXISTS("409-2", "이미 사용 중인 이메일입니다."),
+    USER_ACCESS_DENIED("403-2", "본인의 정보만 조회할 수 있습니다."),
+    USER_NOT_FOUND_OR_DELETED("404-2", "존재하지 않거나 이미 탈퇴한 회원입니다."),
+
+    // Concert
+    CONCERT_NOT_FOUND("404-3", "존재하지 않는 콘서트입니다."),
+
+    // Schedule
+    CONCERT_SCHEDULE_EMPTY("404-4", "등록된 회차가 없습니다."),
+    SCHEDULE_NOT_FOUND("404-5", "존재하지 않는 회차입니다."),
+    INVALID_CONCERT_SCHEDULE("400-1", "해당 콘서트의 회차가 아닙니다."),
+
+    // Seat
+    SEAT_SOLD_OUT("400-2", "이미 매진된 좌석입니다."),
+    SEAT_ALREADY_SOLD("409-3", "이미 판매 완료된 좌석입니다."),
+    SEAT_HELD_BY_OTHER_USER("409-4", "다른 사용자가 선택 중인 좌석입니다."),
+
+    // Ticket
+    TICKET_NOT_FOUND_FOR_USER("404-6", "해당 유저의 티켓이 존재하지 않습니다."),
+    TICKET_ALREADY_CANCELLED("400-3", "이미 취소된 티켓입니다.");
+
+
+    private final String resultCode;
+    private final String message;
+
+    public int getStatusCode() {
+        return Integer.parseInt(resultCode.split("-", 2)[0]);
+    }
+
+}
--- back/src/main/java/com/back/global/exception/ServiceException.java
+++ back/src/main/java/com/back/global/exception/ServiceException.java
@@ -1,18 +1,23 @@
 package com.back.global.exception;
 
 import com.back.global.rsData.RsData;
+import lombok.Getter;
 
+@Getter
 public class ServiceException extends RuntimeException {
-    private final String resultCode;
-    private final String msg;
 
-    public ServiceException(String resultCode, String msg) {
-        super(resultCode + " : " + msg);
-        this.resultCode = resultCode;
-        this.msg = msg;
+    private final ErrorCode errorCode;
+
+    public ServiceException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
     }
 
     public RsData<Void> getRsData() {
-        return new RsData<>(resultCode, msg, null);
+        return new RsData<>(
+                errorCode.getResultCode(),
+                errorCode.getMessage(),
+                null
+        );
     }
 }
