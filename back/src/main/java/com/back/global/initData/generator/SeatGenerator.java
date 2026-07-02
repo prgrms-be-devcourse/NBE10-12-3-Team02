cdc957acd1d54741df1e259b423e16b0a82a22dd
oxcm07 <oxcm0727@gmail.com>
2026-07-02 16:24:41 +0900

    Fix: 좌석수 확장 #86 (#87)

--- back/src/main/java/com/back/global/initData/generator/SeatGenerator.java
+++ back/src/main/java/com/back/global/initData/generator/SeatGenerator.java
@@ -13,18 +13,28 @@ public class SeatGenerator {
 
     private static final List<SeatGrade> GRADES = List.of(
             new SeatGrade("VIP", 150000, "A"),
-            new SeatGrade("R", 120000, "B"),
+            new SeatGrade("VIP", 150000, "B"),
             new SeatGrade("R", 120000, "C"),
-            new SeatGrade("S", 90000, "D"),
-            new SeatGrade("S", 90000, "E"),
-            new SeatGrade("S", 90000, "F"),
-            new SeatGrade("A", 70000, "G"),
-            new SeatGrade("A", 70000, "H"),
-            new SeatGrade("A", 70000, "I"),
-            new SeatGrade("A", 70000, "J")
+            new SeatGrade("R", 120000, "D"),
+            new SeatGrade("R", 120000, "E"),
+            new SeatGrade("R", 120000, "F"),
+            new SeatGrade("S", 90000, "G"),
+            new SeatGrade("S", 90000, "H"),
+            new SeatGrade("S", 90000, "I"),
+            new SeatGrade("S", 90000, "J"),
+            new SeatGrade("S", 90000, "K"),
+            new SeatGrade("S", 90000, "L"),
+            new SeatGrade("A", 70000, "M"),
+            new SeatGrade("A", 70000, "N"),
+            new SeatGrade("A", 70000, "O"),
+            new SeatGrade("A", 70000, "P"),
+            new SeatGrade("A", 70000, "Q"),
+            new SeatGrade("A", 70000, "R"),
+            new SeatGrade("A", 70000, "S"),
+            new SeatGrade("A", 70000, "T")
     );
 
-    private static final int SEATS_PER_ROW = 60;
+    private static final int SEATS_PER_ROW = 30;
 
     public static List<ScheduleSeat> generate(Schedule schedule) {
         List<ScheduleSeat> seats = new ArrayList<>();
