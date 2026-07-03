0c8e610757766f90ad295171485db53256f46698
bangyewon <byewon1111@gmail.com>
2026-07-03 17:21:58 +0900

    Hotfix: 콘서트 상세 이미지 깨짐 수정

--- front/next.config.ts
+++ front/next.config.ts
@@ -3,6 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async rewrites() {
+    return [
+      {
+        source: "/images/logo.png",
+        destination: "/images/logo.svg",
+      },
+      {
+        source: "/images/logo-horizontal.png",
+        destination: "/images/logo-horizontal.svg",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
