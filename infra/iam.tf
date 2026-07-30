f0a2702c2916a2284dbdab80f0c5b83f1d748911
hyunjihun120-beep <hyunjihun120@gmail.com>
2026-07-30 16:04:36 +0900

    feat: ECR lifecycle policy를 untagged 1일 만료 + tagged 최신 10개 유지로 개선

--- infra/iam.tf
+++ infra/iam.tf
@@ -19,11 +19,25 @@ resource "aws_ecr_lifecycle_policy" "app" {
     rules = [
       {
         rulePriority = 1
-        description  = "Keep only the last 5 images"
+        description  = "Expire untagged images older than 1 day"
         selection = {
-          tagStatus   = "any"
-          countType   = "imageCountMoreThan"
-          countNumber = 5
+          tagStatus   = "untagged"
+          countType   = "sinceImagePushed"
+          countUnit   = "days"
+          countNumber = 1
+        }
+        action = {
+          type = "expire"
+        }
+      },
+      {
+        rulePriority = 2
+        description  = "Keep only the last 10 tagged images"
+        selection = {
+          tagStatus      = "tagged"
+          tagPatternList = ["*"]
+          countType      = "imageCountMoreThan"
+          countNumber    = 10
         }
         action = {
           type = "expire"
