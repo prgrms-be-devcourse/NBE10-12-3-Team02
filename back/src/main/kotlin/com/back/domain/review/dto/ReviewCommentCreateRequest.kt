ce11457a5ca30e99354cc8ae2dbaffbeccf78b36
hj-66 <a01088200534@gmail.com>
2026-07-31 11:02:28 +0900

    Feat: 댓글 글자수 제한 추가 #47

--- back/src/main/kotlin/com/back/domain/review/dto/ReviewCommentCreateRequest.kt
+++ back/src/main/kotlin/com/back/domain/review/dto/ReviewCommentCreateRequest.kt
@@ -1,7 +1,10 @@
 package com.back.domain.review.dto
 
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
 
 data class ReviewCommentCreateRequest(
-    @field:NotBlank val content: String
+    @field:NotBlank(message = "댓글을 입력해주세요.")
+    @field:Size(max = 1000, message = "댓글은 1000자 이내로 입력해주세요.")
+    val content: String,
 )
